2ddaf424302212dec63697df903288e9619ceb56
justinbather <justinbather@hotmail.com>
2023-10-31 18:55:31 -0400

    adds verifyUser middleware and /verify endpoint

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-
+const cookieParser = require("cookie-parser");
 const connectDB = require("./config/db");
 
 const auth = require("./routes/auth");
@@ -11,6 +11,7 @@ const app = express();
 const PORT = 8080;
 app.use(cors({ origin: true, credentials: true }));
 app.use(express.json());
+app.use(cookieParser());
 app.use("/auth", auth);
 
 app.listen(PORT, () => console.log("Server running on port 8080!"));
--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const Signup = require("../controllers/auth/Signup");
 const Login = require("../controllers/auth/Login");
+const verifyUser = require("../middleware/verifyUser");
 
 const router = express.Router();
 
@@ -8,7 +9,10 @@ router.post("/signup", Signup);
 router.post("/login", Login);
 router.get("/logout", (req, res) => {
   res.clearCookie("token");
-  return res.sendStatus(200);
+  res.sendStatus(200);
+});
+router.get("/verify", verifyUser, (req, res) => {
+  res.sendStatus(200);
 });
 
 module.exports = router;
