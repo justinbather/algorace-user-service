3a5b322aec956d9267f0421fd540cff5614f2a35
justinbather <justinbather@hotmail.com>
2023-11-13 15:33:33 -0500

    fixes some weird bugs but need to fix the duplicating of users on join

--- socket/socket.js
+++ socket/socket.js
@@ -39,34 +39,69 @@ io.on("connection", (socket) => {
   });
 
 
+
   socket.on("join_lobby", async (data) => {
-    const { username, lobby } = data
-    const lobbyObj = await Lobby.findOne({ name: lobby })
-    if (lobbyObj) {
-      console.log(socket.id)
-      lobbyObj.users.push({ username: username, socketId: socket.id, isReady: false })
-      await lobbyObj.save()
-      socket.join(lobbyObj.name)
-      socket.emit('successful_enter', { lobbyObj })
-      io.to(lobby).emit('user_joined', lobbyObj.users)
-      console.log(lobbyObj)
-    } else {
-      socket.emit('error_joining', "No lobby name found")
+    const { username, lobby } = data;
+    console.log('---------------------join lobby called');
+
+    try {
+      let lobbyObj = await Lobby.findOne({ name: lobby });
+
+      if (lobbyObj) {
+        const userExists = lobbyObj.users.some((user) => user.username === username);
+
+        if (!userExists) {
+          console.log('adding user to lobby');
+          lobbyObj.users.push({ username, socketId: socket.id, isReady: false });
+          lobbyObj = await lobbyObj.save();
+
+          socket.join(lobbyObj.name);
+          socket.emit('successful_enter', { lobbyObj });
+          io.emit('user_joined', lobbyObj);
+        } else {
+          socket.emit('successful_enter', { lobbyObj });
+          io.emit('user_joined', lobbyObj);
+
+          console.log('error: user already in lobby');
+        }
+      } else {
+        socket.emit('error_joining', 'No lobby name found');
+      }
+    } catch (error) {
+      console.error('Error joining lobby:', error);
+      socket.emit('error_joining', 'Internal server error');
     }
   });
 
   socket.on("user_ready", async (data) => {
-    const { username, lobby } = data
-    const lobbyObj = await Lobby.findOne({ name: lobby })
-    const setReady = lobbyObj.users.forEach(user => {
-      if (user.username === username) {
-        user.isReady = true
-        return;
+    console.log('--------------user ready called');
+    const { username, lobby } = data;
+
+    try {
+      let lobbyObj = await Lobby.findOne({ name: lobby });
+
+      if (lobbyObj) {
+        const updatedUsers = lobbyObj.users.map(user => {
+          if (user.username === username) {
+            return { ...user, isReady: true }; // Make a copy with updated isReady
+          }
+          return user;
+        });
+
+        lobbyObj.users = updatedUsers;
+        lobbyObj = await lobbyObj.save();
+
+        io.to(lobby).emit('user_ready', lobbyObj);
+        console.log('sending lobby object after readying: ', lobbyObj);
+      } else {
+        console.log('Error: Lobby not found');
       }
-    });
-    await lobbyObj.save()
-    io.to(lobby).emit('user_ready', lobbyObj.users)
+    } catch (error) {
+      console.error('Error handling user_ready event:', error);
+    }
   })
+
+
 
   socket.on('testing', () => {
     console.log(socket.id)
@@ -75,7 +110,7 @@ io.on("connection", (socket) => {
   socket.on("disconnect", async (lobby) => {
     const count = io.engine.clientsCount;
     console.log("num clients: ", count);
-  });
+  })
 });
 
 module.exports = socketServer
