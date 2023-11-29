710136d4bfc3b8f2b9344fcbd328503c61126e2e
justinbather <justinbather@hotmail.com>
2023-11-29 09:48:42 -0500

    changes description in problemcode schema for better readability on client

--- controllers/auth/Signup.js
+++ controllers/auth/Signup.js
@@ -6,16 +6,28 @@ const Signup = async (req, res) => {
     const { username, password } = req.body;
 
     const user = await User.create({ username, password });
-    const token = createToken(user._id);
-    res.cookie("token", token, {
-      withCredentials: true,
-      httpOnly: false,
-    });
 
-    return res.status(201).json({ user, message: "User created successfully" });
+    const token = createToken(user._id);
+    if (process.env.NODE_ENV === 'production') {
+      res.cookie("token", token, {
+        httpOnly: true,
+        withCredentials: true,
+        domain: 'algorace-user-service-c4f17757eccb.herokuapp.com',
+        sameSite: 'None',
+        secure: true
+      })
+      return res.status(201).json({ message: "User created in successfully" });
+    } else {
+      res.cookie("token", token, {
+        httpOnly: true,
+        withCredentials: true,
+      })
+      return res.status(201).json({ message: "User created in successfully" });
+    }
   } catch (err) {
     return res.status(500).json({ message: "Error creating user", error: err });
   }
+
 };
 
 module.exports = Signup;
--- routes/problems.js
+++ routes/problems.js
@@ -4,12 +4,14 @@ const createProblem = require("../controllers/problems/createProblem");
 const getProblem = require("../controllers/problems/getProblem");
 const getAllProblems = require("../controllers/problems/getAllProblems");
 const createProblemCode = require("../controllers/problems/createProblemCode");
+const updateProblemCode = require("../controllers/problems/updateProblemCode")
 
 const router = express.Router();
 
 router.get("/:problemTitle/:language", verifyUser, getProblem);
 
 router.post("/", verifyUser, createProblem);
+router.patch("/:problemTitle/:problemLang", updateProblemCode)
 router.get("/", verifyUser, getAllProblems);
 
 router.post("/code", verifyUser, createProblemCode);
